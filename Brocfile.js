3a000a83373a3c10b2b4d79328a9059c141b1cad
el-feo <jebcoleman@gmail.com>
2014-08-25 15:35:24 -0700

    move the coreadmin assets to app/styles to work with compass compiler

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,8 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+});
 
 // Javascript Libraries
 app.import('vendor/bootstrap-sass-official/assets/javascripts/bootstrap.js');
